5e97d0f25fddcb955f457548385b870c998aebd4
napolitanobr074 <napolitano74br@gmail.com>
2026-06-01 20:14:43 -0300

    Atualização: Incrementando Sprint 1 v1

--- modulo-01/mini-projeto/alunos/LuisNapolitano_T2/mini_projeto_len_t2.py
+++ modulo-01/mini-projeto/alunos/LuisNapolitano_T2/mini_projeto_len_t2.py
@@ -12,3 +12,44 @@
 # Script 4 - Estatística Descritiva - aplicação de funções estatísticas e criação para coletar parametros da coluna de Número de filhos do cliente; 
 # Script 5 - Relatório e Documentação: construção de contadores do relatório final exibido no terminal, finalização do README.md com reflexao teórica e submissão do link AVA
 # Script 6 - Versionamento: Envio dos arquivos (script + README.md + df_limpo), via Git para o repositório da turma no Githu.
+
+# =====================================================================
+# SPRINT 1: IMPORTAÇÃO DOS DADOS
+# =====================================================================
+print("\n" * 2)
+print("******** IMPORTAÇÃO DOS DADOS - SPRINT 1 ********")
+
+# Importação das bibliotecas necessárias para o Miniprojeto
+import pandas as pd
+import numpy as np
+
+print("ln - Bibliotecas importadas com sucesso!")
+print("\n")
+
+CaminhoBaseVarejo = r"modulo-01/base_do_projeto/Base Varejo.csv"
+
+df = pd.read_csv(
+    CaminhoBaseVarejo,
+    encoding="utf-8",
+    sep=";",
+    decimal=",",
+)
+
+print("ln - BANCO DE DADOS (dataset) ACESSADO COM SUCESSO!")
+print("\n" * 2)
+
+# Consultando a base de dados como foi importada para entendimento e analise de limpeza e formatação
+# 1. consultando a quantidade de linhas e colunas
+print(f"O nosso df possui {df.shape[0]} linhas e {df.shape[1]} colunas.")
+print("\n" * 2)
+
+# 2. Consultando as 5 primeiras linhas da base de dados ...
+print("ln - Exibindo as 5 primeiras linhas do df, informações gerais e formato do df:")
+print(df.head(5))
+print("\n" * 2)
+
+# 3. Consultado as informações derais da base de dados ...
+print("ln - Exibindo as informações gerais do dataframe:")
+print(df.info())
+print("\n" * 2)
+
