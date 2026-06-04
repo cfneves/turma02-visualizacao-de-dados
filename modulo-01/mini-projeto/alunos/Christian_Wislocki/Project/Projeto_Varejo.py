dc2b07e4a8570b10fd642e0d1c8fef221e975042
christianwislockioli-rgb <christian.wislocki.oli@gmail.com>
2026-06-04 17:07:20 -0300

    Envio dos readme do miniprojeto
    Nao enviei os readmes diretamente para o repositorio da turma, acabei apenas deixano dentro do meu repositorio no github

--- modulo-01/mini-projeto/alunos/Christian_Wislocki/Project/Projeto_Varejo.py
+++ modulo-01/mini-projeto/alunos/Christian_Wislocki/Project/Projeto_Varejo.py
@@ -21,11 +21,11 @@ df_copy['DATA'] = pd.to_datetime(df['DATA'], dayfirst=True)
 
 # %%
 # recolhendo amostras
-print(df_copy.head(10))
+df_copy.head(10)
 
 # %%
 # verificaçao de tipos de dados
-print(df_copy.info())
+df_copy.info()
 
 # %% [markdown]
 # ## Identificando Problemas
@@ -38,7 +38,7 @@ print((df_copy.isnull().sum() / len(df_copy) * 100).round(2))
 
 # %%
 # verificando duplicatas
-print(df_copy.duplicated().sum())
+df_copy.duplicated().sum()
 
 
 # %% [markdown]
@@ -56,7 +56,7 @@ print(df_copy.duplicated().sum())
 duplicatas = df_copy[df_copy.duplicated(keep=False)]
 
 # como queremos descobrir se a questao das duplicatas tem algo haver com o id do cliente e o id do produto as ordemos dessa forma para  que facilite nossa analise
-print(duplicatas.sort_values(by=['CO_ID', 'PR_ID']).head(10))
+duplicatas.sort_values(by=['CO_ID', 'PR_ID']).head(10)
 
 
 # %%
@@ -64,7 +64,7 @@ print(duplicatas.sort_values(by=['CO_ID', 'PR_ID']).head(10))
 coluna = df_copy.columns.tolist()
 df_copy = df_copy.groupby(coluna, dropna=False).size().reset_index(name='QTD_PR')
 
-print(df_copy[df_copy['CO_ID'] == '1000'].head(10))
+df_copy[df_copy['CO_ID'] == '1000'].head(10)
 
 # %%
 # removendo colunas inteiramente vazias, pois nao fara sentido ter elas dentro da nossa base de dados que posteriormente sera utilizada por alguem
@@ -82,7 +82,7 @@ df_copy['CL_EC'] = df_copy['CL_EC'].map(map_df)
 
 # %%
 # vendo quantas linhas temos valores nulos/ausentes ou #N/D
-print(df_copy[df_copy['PR_CAT'] == '#n/d'].shape[0])
+df_copy[df_copy['PR_CAT'] == '#n/d'].shape[0]
 
 # %%
 # vamos substituir a string '#N/D' por valores nulos, pois na etapa de verificaçao nao foi constatado
@@ -107,7 +107,7 @@ df_copy['PR_CAT'] = df_copy['PR_CAT'].astype('category')
 
 # %%
 # filtraremos apenas colunas de texto (object), categorias (category) e texto (strings)
-print(df_copy.describe(include=['object', 'category', 'str']))
+df_copy.describe(include=['object', 'category', 'str'])
 
 # %%
 # obtendo as principais estatisticas descritivas dos dados
@@ -128,7 +128,7 @@ def descritivas(data):
 
 # %%
 # analise descritiva da coluna CL_FHL (quantidade de filhos por cliente)
-print(descritivas(df_copy[['CL_FHL']]))
+descritivas(df_copy[['CL_FHL']])
 
 # %% [markdown]
 # ## Analise Exploratorio e Agrupamentos
@@ -145,7 +145,7 @@ volume_por_genero = volume_por_genero.rename(columns={'QTD_PR': 'Volume_Total'})
 
 volume_por_genero = volume_por_genero.sort_values(by='Volume_Total', ascending=False)
 
-print(volume_por_genero)
+volume_por_genero
 
 # %%
 # grafico do volume por genero
@@ -168,7 +168,7 @@ drill_down_genero_cat = df_copy.pivot_table(
     aggfunc='sum'         
 )
 
-print(drill_down_genero_cat)
+drill_down_genero_cat
 
 # %%
 # quem compra mais em volume de produtos?
@@ -177,7 +177,7 @@ volume_cliente = volume_cliente.rename(columns={'QTD_PR': 'Qtd_Produtos'})
 
 top_clientes_volume = volume_cliente.sort_values(by='Qtd_Produtos', ascending=False).head(10)
 
-print(top_clientes_volume)
+top_clientes_volume
 
 # %%
 # verificaçao de clientes padrao ouro
@@ -185,7 +185,7 @@ print(top_clientes_volume)
 volume_cliente['Qtd_Produtos'].describe()
 percentis = volume_cliente['Qtd_Produtos'].quantile([0.10, 0.25, 0.50, 0.75, 0.90, 0.95, 0.99])
 
-print(percentis)
+percentis
 
 # %% [markdown]
 # #### Quais categorias vendem mais (em volume total)?
@@ -198,7 +198,7 @@ vendas_categoria = vendas_categoria.rename(columns={'QTD_PR': 'Volume_Total_Vend
 # ordenando as categorias
 vendas_categoria = vendas_categoria.sort_values(by='Volume_Total_Vendido', ascending=False)
 
-print(vendas_categoria)
+vendas_categoria
 
 # %%
 # grafico do ranking das vendas
@@ -226,7 +226,7 @@ produtos_da_categoria = df_campea.groupby('PR_NOME')['QTD_PR'].sum().reset_index
 produtos_da_categoria = produtos_da_categoria.rename(columns={'QTD_PR': 'Volume'})
 produtos_da_categoria = produtos_da_categoria.sort_values(by='Volume', ascending=False).head(10)
 
-print(produtos_da_categoria)
+produtos_da_categoria
 
 # %%
 
@@ -256,7 +256,7 @@ vendas_tempo = df_copy.groupby('Ano_Mes')['QTD_PR'].sum().reset_index()
 vendas_tempo['Ano_Mes'] = vendas_tempo['Ano_Mes'].astype(str)
 
 # abaixo iremos fazer um grafico de linha para termos noçao de como variam as vendas ao longo do tempo
-plt.figure(figsize=(22, 5))
+plt.figure(figsize=(12, 5))
 sns.lineplot(data=vendas_tempo, x='Ano_Mes', y='QTD_PR', marker='o')
 plt.title('Variacao do Volume de Vendas ao Longo do Tempo')
 plt.xlabel('Mes e Ano')
@@ -274,7 +274,7 @@ df_copy['Nome_Dia'] = df_copy['Dia_Semana'].map(dias_map)
 
 # agrupando o volume de vendas por dia da semana
 vendas_por_dia = df_copy.groupby('Nome_Dia')['QTD_PR'].sum().reindex(list(dias_map.values())).reset_index()
-print(vendas_por_dia)
+vendas_por_dia
 
 # %%
 # modificando a coluna ano mes para string, pois so assim dara para fazero grafico para melhor visualizaçao
@@ -284,7 +284,7 @@ df_copy['Ano_Mes'] = df_copy['DATA'].dt.to_period('M').astype(str)
 vendas_tempo_segmento = df_copy.groupby(['Ano_Mes', 'CL_SEG'])['QTD_PR'].sum().reset_index()
 
 # criaçao do gráfico de linha por segmento
-plt.figure(figsize=(22, 5))
+plt.figure(figsize=(14, 6))
 sns.lineplot(data=vendas_tempo_segmento, x='Ano_Mes', y='QTD_PR', hue='CL_SEG', marker='o', palette='Set1')
 
 plt.title('Variação das Vendas ao Longo do Tempo por Segmento de Cliente')
@@ -296,43 +296,3 @@ plt.tight_layout()
 plt.show()
 
 
-print("""
-=========================================================
-🎯 CONCLUSÕES E INSIGHTS FINAIS
-=========================================================
-      
-      1. Perfil do Cliente: A maioria absoluta da nossa carteira de clientes é composta por pessoas sem filhos (50%), caracterizando famílias de pequeno porte (média de 1,15 filhos).
-
-      2. Motor de Vendas: A categoria "Alimentos" é a campeã isolada de volume, sendo puxada principalmente por produtos como Presunto Cozido e Sardinha.
-
-      3. Sazonalidade (Dias da Semana): Identificamos picos de vendas na Sexta-feira, Domingo (representando 15,80% cada) e na Quarta-Feira (18,50%).
-
-      4. Próximos Passos (Problemas Remanescentes): Seria recomendado acionar o setor de marketing para entender o baixo volume atípico de vendas aos Sábados (~11,37%) e investigar possíveis indisponibilidades de sistema ou falta de campanhas nesse dia.
-
-      5. Clientes VIP (Top 10%): O nosso "Padrão Ouro" engloba clientes que compraram mais de 1.112 itens. Este grupo deve ser o alvo principal de ações de retenção e programas de fidelidade, dado o seu alto valor para o negócio.
-
-      6. Atenção a Outliers: O maior comprador adquiriu 1.772 produtos, um valor significativamente acima do corte do percentil 99% (1.396). Isso levanta uma hipótese de negócio: podemos ter clientes corporativos (B2B) atuando em nossa base de varejo padrão.
-
-      7. O Núcleo da Base: Metade dos nossos clientes (50%) consome entre 657 e 977 itens, indicando um comportamento de compra consistente e uma oportunidade clara para campanhas de cross-sell (venda cruzada).
-
-      8. Liderança Feminina Contínua: As clientes do gênero feminino lideram o volume de compras em absolutamente todas as categorias.
-
-      9. Proporção Estável: Independentemente do tipo de produto, a divisão de consumo é extremamente homogênea. Em todas as categorias, as mulheres representam uma fatia que varia entre 51,5% e 53%, enquanto os homens respondem pelo restante (cerca de 47% a 48,5%).
-
-      10. O "Porquê": Isso nos diz que, nesta base de varejo específica, o comportamento de compra entre gêneros é muito similar e não há nenhum nicho de produto polarizado
-
-      11. Evite Segmentação Extrema por Gênero: Como as proporções variam muito pouco (no máximo ~5% de diferença entre os gêneros), gastar muito orçamento de Marketing tentando criar campanhas exclusivamente masculinas ou femininas para categorias específicas terá um ROI (Retorno sobre Investimento) baixo. O gênero não é a principal alavanca de conversão aqui.
-
-      12. A Estratégia de Cross-Selling: O verdadeiro abismo nos dados está no volume entre categorias. A categoria de "Alimentos" tem um volume quase 3 vezes maior do que "Higiene" e "Limpeza" somadas.
-
-      13. Ação Prática: A estratégia ideal é usar "Alimentos" como isca. Como todo mundo (homens e mulheres) compra muito alimento, o sistema do varejo ou aplicativo deve sugerir combos promocionais de venda cruzada (cross-selling). Ex: "Compre X em Alimentos e ganhe 15% de desconto no departamento Pet ou de Acessórios". Isso alavanca os produtos que têm menor saída aproveitando o fluxo pesado da categoria principal.
-
-=========================================================
-      PROBLEMAS PARA O GESTOR
-=========================================================
-      
-      1. Removemos cerca de 3.228 valores nulos da nossa base, pedir orientação do responsavel para tomarmos a melhor decisao possivel, deixar em aberto a questao do nulos, pois pode <br> ter a chance de recuperar, caso cruzemos com alguma outra base de dados da nossa empresa
-
-      2. Relatar sobre a criaçao da nova coluna QTD_PR, para justificar a "perda" de linhas da nossa base de dados
-
-""")
